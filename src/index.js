4aec513932790da4b3baddc448b303e05d50a7dc
Luis Farzati <lfarzati@gmail.com>
2016-08-28 03:03:05 -0300

    added Array.from polyfill

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
-import esPadStart from 'string.prototype.padstart'
+import stringPadStart from 'string.prototype.padstart'
+import arrayFrom from 'array.from'
 import 'string.prototype.codepointat'
 import 'string.fromcodepoint'
 
-esPadStart.shim()
+stringPadStart.shim()
+arrayFrom.shim()
 
 ReactDOM.render(<App />, document.getElementById('main'))
